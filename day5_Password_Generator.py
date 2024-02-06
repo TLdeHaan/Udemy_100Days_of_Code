1c6b874b440cc0d9cd37312b75c5320f9284bd07
Timo <timo_de_haan@hotmail.com>
2024-02-06 22:35:01 +0100

    Day 5 complete

--- day5_Password_Generator.py
+++ day5_Password_Generator.py
@@ -8,12 +8,12 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 
-#For loop with Range
+# For loop with Range
 for number in range(1, 11, 3):
     print(number)
 
 total = 0
-for number in range (1, 101):
+for number in range(1, 101):
     total += number
 print(total)
 
@@ -39,13 +39,13 @@ print(f"average height = {average_height}")
 # Input a list of student scores
 student_scores = input().split()
 for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+    student_scores[n] = int(student_scores[n])
 
 # Write your code below this row 👇
 high_score = 0
 for score in student_scores:
     if score > high_score:
-      high_score = score
+        high_score = score
 print(f"The highest score in the class is: {high_score}")
 
 '''LESSON 18 DAY 5 - ADDING EVEN NUMBERS'''
@@ -55,7 +55,7 @@ target = int(input()) # Enter a number between 0 and 1000
 # Write your code here 👇
 total = 0
 for number in range(2, target + 1, 2):
-  total += number
+    total += number
 print(total)
 
 # alternative
@@ -69,22 +69,24 @@ print(total)
 # Write your code here 👇
 target = 100
 for number in range(1, target + 1):
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
-  elif number % 3 == 0:
-    print("Fizz")
-  elif number % 5 == 0:
-    print("Buzz")
-  else:
-    print(number)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
 '''PASSWORD GENERATOR'''
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
