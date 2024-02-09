05b9f39dfe47dd93711735c74e8709edb3337a1e
Timo de Haan <51243064+TLdeHaan@users.noreply.github.com>
2024-02-09 15:08:54 +0100

    Day 7 complete - Minor update

--- day7_Hangman.py
+++ day7_Hangman.py
@@ -4,8 +4,12 @@ import random
 from day7_Hangman_art import logo, stages
 from day7_Hangman_words import word_list
 
-'''HANGMAN'''
+'''THEORY'''
 # Always try to start with a flowchart
+# Write down the components of the flow chart in your script as comments
+# Use small samples to test codes and only add true data at the end
+
+'''HANGMAN'''
 lives = len(stages) - 1
 chosen_word = random.choice(word_list)
 # print(chosen_word)
@@ -14,12 +18,14 @@ for i in range(len(chosen_word)):
     display.append("_")
     # or display += "_"
 # print(display)
+guessed = []
 end_of_game = False
 
 print(logo)
 while not end_of_game:
-    guess = input("Guess a letter:").lower()
+    print(display)
 
+    guess = input("Guess a letter:").lower()
     if guess in display:
         print(f"You already guessed {guess}. Try again.")
     elif guess in chosen_word:
@@ -28,15 +34,16 @@ while not end_of_game:
                 display[i] = chosen_word[i]
     else:
         lives -= 1
-        print(f"You've guessed {guess} which is not in the word:")
+        print(f"The letter \'{guess}\' is not in the word.")
+        guessed += guess
         print(stages[lives])
-    print(display)
 
     if "_" not in display:
         end_of_game = True
         print("You win!")
-
-    if lives == 0:
+    elif lives == 0:
         end_of_game = True
-        print(stages[lives])
         print("You lose!")
+        print(f"The word was {chosen_word}")
+    else:
+        print(f"Letters not in the word: {guessed}")
